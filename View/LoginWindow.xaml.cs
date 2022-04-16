33855ccfaa6d9ec8d94f02f84a70745abcbf0e70
Cristian George <cfieraru29@yahoo.com>
2022-04-17 01:32:15 +0300

    Add project files.

---
+++ View/LoginWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Hangman.View
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
